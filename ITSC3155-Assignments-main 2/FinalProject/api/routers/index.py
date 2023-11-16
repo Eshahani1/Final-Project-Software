d942b8f2d056353b4d75bad8175a85cce7b83371
sania-k <114709835+sania-k@users.noreply.github.com>
2023-11-16 12:34:46 -0500

    added all tables to router index

--- ITSC3155-Assignments-main 2/FinalProject/api/routers/index.py
+++ ITSC3155-Assignments-main 2/FinalProject/api/routers/index.py
@@ -1,8 +1,12 @@
-from . import orders, order_details, guests
+from . import orders, order_details, guests, payments, promos, recipes, resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(guests.router)
+    app.include_router(payments.router)
+    app.include_router(promos.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
 
