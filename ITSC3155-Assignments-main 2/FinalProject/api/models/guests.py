91f9d299d8279140f6ba3f08dbeba568a7bc623e
Eshahani1 <112349703+Eshahani1@users.noreply.github.com>
2023-11-16 16:38:42 -0500

    fixes

--- ITSC3155-Assignments-main 2/FinalProject/api/models/guests.py
+++ ITSC3155-Assignments-main 2/FinalProject/api/models/guests.py
@@ -6,12 +6,12 @@ class Guest(Base):
     __tablename__ = "guests"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, index=True, nullable=False)
+    name = Column(String(18), index=True, nullable=False)
     payment_id = Column(Integer, ForeignKey("payments.id"))
     order_id = Column(Integer, ForeignKey("orders.id"))
     promo_id = Column(Integer, ForeignKey("promos.id"))
-    phone_number = Column(String, index=True, nullable=False)
-    address = Column(String, nullable=False)
+    phone_number = Column(String(20), index=True, nullable=False)
+    address = Column(String(50), nullable=False)
 
     payment = relationship("Payment", back_populates="guests")
     order = relationship("Order", back_populates="guests")
