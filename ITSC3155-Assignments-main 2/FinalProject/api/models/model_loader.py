4ed6032ba6d05da1ae94dd0e0467670c697c3253
sania-k <114709835+sania-k@users.noreply.github.com>
2023-11-16 12:47:18 -0500

    added all tables to model_loader

--- ITSC3155-Assignments-main 2/FinalProject/api/models/model_loader.py
+++ ITSC3155-Assignments-main 2/FinalProject/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, guests
+from . import guests, menu, order_details, orders, payments, promos, recipes, resources
 
 from ..dependencies.database import engine
 
@@ -7,6 +7,8 @@ def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
     recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     guests.Base.metadata.create_all(engine)
+    menu.Base.metadata.create_all(engine)
+    payments.Base.metadata.create_all(engine)
+    promos.Base.metadata.create_all(engine)
