172ec2670ba65239417c636eb6adafc8f7e493f7
sania-k <114709835+sania-k@users.noreply.github.com>
2023-11-18 23:10:12 -0500

    fixed imports

--- ITSC3155-Assignments-main 2/FinalProject/api/routers/resources.py
+++ ITSC3155-Assignments-main 2/FinalProject/api/routers/resources.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, FastAPI, status, Response
 from sqlalchemy.orm import Session
-from ..controllers import orders as controller
+from ..controllers import resources as controller
 from ..schemas import resources as schema
 from ..dependencies.database import engine, get_db
 
