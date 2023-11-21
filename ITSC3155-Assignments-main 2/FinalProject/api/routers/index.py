ea59a69ba7c5adde95984d363b58e941366fd25d
sania-k <114709835+sania-k@users.noreply.github.com>
2023-11-21 14:04:22 -0500

    changing order tables are loaded
    did this so the api is easier to navigate

--- ITSC3155-Assignments-main 2/FinalProject/api/routers/index.py
+++ ITSC3155-Assignments-main 2/FinalProject/api/routers/index.py
@@ -3,13 +3,13 @@ from . import (orders, order_details, guests, payments, promos,
 
 
 def load_routes(app):
-    app.include_router(orders.router)
-    app.include_router(order_details.router)
-    app.include_router(guests.router)
     app.include_router(payments.router)
     app.include_router(promos.router)
+    app.include_router(promo_details.router)
+    app.include_router(guests.router)
+    app.include_router(orders.router)
+    app.include_router(order_details.router)
+    app.include_router(menu_items.router)
     app.include_router(recipes.router)
     app.include_router(resources.router)
-    app.include_router(menu_items.router)
-    app.include_router(promo_details.router)
 
